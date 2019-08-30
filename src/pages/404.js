f1bce0ddb2644d89afbec8ef482033dfaaa3e616
Asa Smith <smith.asa.la@gmail.com>
2019-08-29 22:46:40 -0400

    404 updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
@@ -6,9 +8,33 @@ import SEO from '../components/seo'
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <StyledContainer>
+      <p
+        style={{
+          fontSize: `1.5rem`,
+        }}
+      >
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
+      <Link className="h2" to="/" as="a">
+        Country roads, take me home...
+      </Link>
+    </StyledContainer>
   </Layout>
 )
+
+const StyledContainer = styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 90vh;
+
+  a {
+    &:hover {
+      border-bottom: 1px solid;
+    }
+  }
+`
 
 export default NotFoundPage
