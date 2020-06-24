447e23ae293c340c44dce59d9034fd6dca547b07
asasmith <smith.asa.la@gmail.com>
2020-06-24 15:57:24 -0400

    setup createPage function for case studies

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -5,3 +5,41 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+  
+    const result = await graphql(`
+        {
+            allSanityCaseStudy {
+                edges {
+                    node {
+                        title
+                        Slug {
+                            current
+                        }
+                        id
+                    }
+                }
+            } 
+        }
+    `)
+  
+    if (result.errors) {
+        throw result.errors
+    }
+  
+    const caseStudies = result.data.allSanityCaseStudy.edges || []
+
+    caseStudies.forEach((edge, index) => {
+        const path = `/case-studies/${edge.node.Slug.current}`
+
+        console.log(`page created at ${path}`)
+  
+        createPage({
+            path,
+            component: require.resolve("./src/templates/case-study.js"),
+            context: { slug: edge.node.Slug.current },
+        })
+    })
+}
