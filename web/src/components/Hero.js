5c9f17c8718dabd76738970ea514b17251be705e
asasmith <smith.asa.la@gmail.com>
2020-09-26 17:16:04 -0400

    create new hero component

---
+++ web/src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+import Img from 'gatsby-image'
+
+const StyledSection = styled.section`
+    ${tw`container flex flex-col justify-end content-center min-h-screen mx-auto`}
+`
+
+const StyledHeading = styled.h1`
+    ${tw`max-w-5xl mx-auto text-center`}
+`
+
+export default function Hero ({ heading, image }) {
+    return (
+        <StyledSection>
+            <StyledHeading>{ heading }</StyledHeading>
+            <Img
+                fluid={image.childImageSharp.fluid}
+                className={`sm:w-full xl:w-4/5 mt-20 mx-auto`}
+            />
+        </StyledSection>
+    )
+}
