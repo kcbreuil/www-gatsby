fc223988aafc4726fde7554052fda8efbe3c0350
asasmith <smith.asa.la@gmail.com>
2020-09-26 14:53:52 -0400

    add tailwindcss

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -60,6 +60,18 @@ module.exports = {
         icon: `src/images/Backroom-Mark-White.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [require('tailwindcss')('./tailwind.config.js')],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
