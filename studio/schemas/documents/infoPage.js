31aa66898ced776f265f3180faf15d40bdfe766a
asasmith <smith.asa.la@gmail.com>
2020-10-04 14:18:33 -0400

    setup up info page document

---
+++ studio/schemas/documents/infoPage.js
@@ -0,0 +1,27 @@
+export default {
+    name: 'infoPage',
+    type: 'document',
+    title: 'Info Page',
+    fields: [
+        {
+            name: 'pageTitle',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'team',
+            title: 'Team',
+            type: 'array',
+            of: [
+                {
+                    type: 'reference',
+                    to: [
+                        {
+                            type: 'team',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+}
