ea8b42dfb6828a5a104549a223f8279549b9907a
asasmith <smith.asa.la@gmail.com>
2020-10-05 00:26:49 -0400

    remove placeholder image

--- web/src/components/Hero.js
+++ web/src/components/Hero.js
@@ -5,21 +5,21 @@ import tw from 'twin.macro';
 import Img from 'gatsby-image'
 
 const StyledSection = styled.section`
-    ${tw`container flex flex-col justify-end content-center min-h-screen mb-20`}
+    ${tw`container flex flex-col justify-center content-center min-h-screen mb-10`}
 `
 
 const StyledHeading = styled.h1`
-    ${tw`max-w-5xl mx-auto text-center text-h1`}
+    ${tw`max-w-5xl mx-auto text-center text-h1 mt-16`}
 `
 
 export default function Hero ({ heading, image }) {
     return (
         <StyledSection>
             <StyledHeading>{ heading }</StyledHeading>
-            <Img
+            {/* <Img
                 fluid={image.childImageSharp.fluid}
                 className={`sm:w-full xl:w-4/5 mt-20 mx-auto`}
-            />
+            /> */}
         </StyledSection>
     )
 }
