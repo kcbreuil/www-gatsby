d7521df090fec2d4a410f0d12a1a32a9f0eb1dd2
asasmith <smith.asa.la@gmail.com>
2020-10-06 19:57:30 -0400

    add NewNav, convert file to 4 space indent

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -15,126 +15,133 @@ import * as fonts from '../fonts'
 import NavModal from './NavModal'
 import Header from './Header'
 import './layout.css'
+import NewNav from './NewNav'
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Galano Grotesque';
-    font-style: normal;
-    font-weight: normal;
-    src: local('Galano Grotesque'), url('${
-      fonts.GalanoGrotesqueReg
-    }') format('OpenType');
-  }
+    @font-face {
+        font-family: 'Galano Grotesque';
+        font-style: normal;
+        font-weight: normal;
+        src: local('Galano Grotesque'), url('${
+        fonts.GalanoGrotesqueReg
+        }') format('OpenType');
+    }
 
-  @font-face {
-    font-family: 'Galano Grotesque';
-    font-style: normal;
-    font-weight: bold;
-    src: local('Galano Grotesque'), url('${
-      fonts.GalanoGrotesqueBold
-    }') format('OpenType');
-  }
+    @font-face {
+        font-family: 'Galano Grotesque';
+        font-style: normal;
+        font-weight: bold;
+        src: local('Galano Grotesque'), url('${
+        fonts.GalanoGrotesqueBold
+        }') format('OpenType');
+    }
 
-  @font-face {
-    font-family: 'Galano Grotesque',
-    font-style: italic;
-    font-weight: normal;
-    src: local('Galano Grotesque'), url('${
-      fonts.GalanoGrotesqueItalic
-    }') format('OpenType')
-  }
+    @font-face {
+        font-family: 'Galano Grotesque',
+        font-style: italic;
+        font-weight: normal;
+        src: local('Galano Grotesque'), url('${
+        fonts.GalanoGrotesqueItalic
+        }') format('OpenType')
+    }
 
-  body {
-    color: #4e5859;
-    font-family: 'Galano Grotesque';
-    font-feature-settings: "kern", "liga", "clig", "calt";
-    font-kerning: normal;
-    font-size: 1rem;
-    font-weight: normal;
-    line-height: 1.5;
-    word-wrap: break-word;
-  }
+    body {
+        color: #4e5859;
+        font-family: 'Galano Grotesque';
+        font-feature-settings: "kern", "liga", "clig", "calt";
+        font-kerning: normal;
+        font-size: 1rem;
+        font-weight: normal;
+        line-height: 1.5;
+        word-wrap: break-word;
+    }
 
-  h1,
-  h2,
-  h3,
-  h4 {
-    font-weight: bold;
-    line-height: 1;
-  }
+    h1,
+    h2,
+    h3,
+    h4 {
+        font-weight: bold;
+        line-height: 1;
+    }
 
-  h1,
-  .h1 {
-    font-size: 3.157rem;
-  }
+    h1,
+    .h1 {
+        font-size: 3.157rem;
+    }
 
-  h2,
-  .h2 {
-    font-size: 2.369rem;
-  }
+    h2,
+    .h2 {
+        font-size: 2.369rem;
+    }
 
-  h3,
-  .h3 {
-    font-size: 1.777rem;
-  }
+    h3,
+    .h3 {
+        font-size: 1.777rem;
+    }
 
-  h4,
-  .h4 {
-    font-size: 1.333rem;
-  }
+    h4,
+    .h4 {
+        font-size: 1.333rem;
+    }
 
-  a {
-    text-decoration: none;
-  }
+    a {
+        text-decoration: none;
+    }
 `
 
 export default class Layout extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      navActive: false,
+    constructor(props) {
+        super(props)
+        this.state = {
+            navActive: true,
+        }
+
+        this.handleNavClick = this.handleNavClick.bind(this)
     }
 
-    this.handleNavClick = this.handleNavClick.bind(this)
-  }
+    handleNavClick() {
+        const { navActive } = this.state
 
-  handleNavClick() {
-    console.log('clicked the thing')
-    const { navActive } = this.state
-    this.setState({
-      navActive: !navActive,
-    })
-  }
+        console.log('clicked')
+        this.setState({
+            navActive: !navActive,
+        })
+    }
 
-  render() {
-    const { children, data } = this.props
-    const { navActive } = this.state
-    return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
-          }
-        `}
-        render={data => (
-          <>
-            <GlobalStyle />
-            <NavModal navActive={navActive} />
-            <Header
-              siteTitle={data.site.siteMetadata.title}
-              navActive={navActive}
-              handleNavClick={this.handleNavClick}
-            />
-            <main>{children}</main>
-          </>
-        )}
-      />
-    )
-  }
+    render() {
+        console.log(navActive);
+        const { children, data } = this.props
+        const { navActive } = this.state
+        return (
+        <StaticQuery
+            query={graphql`
+                query SiteTitleQuery {
+                    site {
+                        siteMetadata {
+                            title
+                        }
+                    }
+                }
+            `}
+            render={data => (
+            <>
+                <GlobalStyle />
+                {/* <NavModal navActive={navActive} />
+                <Header
+                siteTitle={data.site.siteMetadata.title}
+                navActive={navActive}
+                handleNavClick={this.handleNavClick}
+                /> */}
+                <NewNav
+                    handleNavClick={this.handleNavClick}
+                    navActive={navActive}
+                />
+                <main>{children}</main>
+            </>
+            )}
+        />
+        )
+    }
 }
 
 Layout.propTypes = {
