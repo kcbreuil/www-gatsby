a9c436cc38445f6b9f49963d211125c6d84ede56
asasmith <smith.asa.la@gmail.com>
2020-10-06 20:13:36 -0400

    update html, line-heights

--- web/src/components/ServiceItem.js
+++ web/src/components/ServiceItem.js
@@ -5,18 +5,24 @@ import tw from 'twin.macro';
 // todo: sort out line heights, need to extend theme
 
 const StyledHeading = styled.h3`
-    ${tw`capitalize font-bold text-3xl mb-2`}
+    ${tw`capitalize font-bold text-3xl mb-3`}
 `
 
-const StyledListItem = styled.div`
-    ${tw`text-2xl`}
+const StyledList = styled.ul`
+    ${tw`mb-0`}
+`
+
+const StyledListItem = styled.li`
+    ${tw`text-2xl mb-0 leading-7`}
 `
 
 export default function ServiceItem ({ service }) {
     return (
         <div>
             <StyledHeading>{service.serviceCategory}</StyledHeading>
-            {service.serviceItems.map(( item, index ) => <StyledListItem key={index}>{item}</StyledListItem> )}
+            <StyledList>
+                {service.serviceItems.map(( item, index ) => <StyledListItem key={index}>{item}</StyledListItem> )}
+            </StyledList>
         </div>
     )
 }
