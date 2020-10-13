a99af8653c79f56635d3749358caf555ebcb2426
asasmith <smith.asa.la@gmail.com>
2020-10-13 18:31:23 -0400

    formatting

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,25 +5,25 @@ module.exports = {
   },
     purge: [],
     theme: {
-    container: {
-        center: true,
-    },
-    extend: {
-        colors: {
-            bckrmGreen: '#2E444F',
-            bckrmGrey: '#4E5859',
-    },
-    fontSize: {
-        'h1': '3.75rem',
-    },
-    gridRowStart: {
-        '8': '8',
-    },
-    lineHeight: {
-            '11': '3rem',
-            '12': '4rem'
+        container: {
+            center: true,
         },
-    },
+        extend: {
+            colors: {
+                bckrmGreen: '#2E444F',
+                bckrmGrey: '#4E5859',
+            },
+            fontSize: {
+                'h1': '3.75rem',
+            },
+            gridRowStart: {
+                '8': '8',
+            },
+            lineHeight: {
+                '11': '3rem',
+                '12': '4rem'
+            },
+        },
     },
     variants: {},
     plugins: [],
