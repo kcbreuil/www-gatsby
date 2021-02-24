36a8b1382a4046215515d8e96eaa6b9b22e8d0de
asmith02 <asa.smith@verizonmedia.com>
2021-02-24 18:24:16 -0500

    Update tailwind, postcss, twin.macro

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,33 +1,11 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-    purge: [],
+    purge: ['./src/**/*.js', './src/**/*.jsx'],
     theme: {
+        extend: {},
         container: (theme) => ({
             center: true,
             padding: theme('spacing.5'),
         }),
-        extend: {
-            colors: {
-                bckrmGreen: '#2E444F',
-                bckrmGrey: '#4E5859',
-            },
-            fontSize: {
-                'h1': '3.75rem',
-            },
-            gridRowStart: {
-                '8': '8',
-            },
-            height: {
-                '3/4': '75vh',
-            },
-            lineHeight: {
-                '11': '3rem',
-                '12': '4rem'
-            },
-        },
     },
     variants: {},
     plugins: [],
