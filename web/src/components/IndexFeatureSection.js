b6ef664bb4bedb6fd611d4b25789ae2ec4e6679c
asmith02 <asa.smith@verizonmedia.com>
2021-02-25 20:08:13 -0500

    Update features layout

--- web/src/components/IndexFeatureSection.js
+++ web/src/components/IndexFeatureSection.js
@@ -9,26 +9,30 @@ import MarkAbbr from './svgs/mark-abbr';
 
 
 const Grid = styled.section`
-    ${tw`container grid grid-cols-1 md:grid-cols-12 gap-y-16 md:gap-16 my-10 min-h-screen`}
+    ${tw`container grid grid-cols-1 md:grid-cols-12 auto-rows-auto gap-y-8 lg:gap-x-16 lg:gap-y-0 my-20 xl:mb-40`}
+`
+
+const MarkWrapper = styled.div`
+    ${tw`md:col-span-5 lg:col-span-6 flex justify-center row-start-1 lg:flex lg:justify-center`}
 `
 
 const ImageOne = styled.div`
-    ${tw`md:col-start-7 md:col-end-12 md:row-start-2`}
+    ${tw`md:col-start-8 md:col-end-13 lg:col-start-7 row-start-2 md:row-start-1 lg:row-start-2`}
 `
 
 const ImageTwo = styled.div`
-    ${tw`md:col-span-6 md:flex md:flex-col md:justify-center row-start-3 md:row-start-2`}
+    ${tw`md:col-span-10 md:col-start-2 lg:col-span-6 row-start-4 md:row-start-3 lg:row-start-2 lg:flex lg:flex-col lg:justify-end lg:transform lg:transition lg:translate-y-1/3`}
 `;
 
-const MarkWrapper = styled.div`
-    ${tw`col-span-6 flex justify-center`}
-`
+const QuoteWrapper = styled.div`
+    ${tw`md:col-span-10 md:col-start-2 lg:col-start-7 lg:col-end-13 row-start-3 md:row-start-2 lg:row-start-3`}
+`;
 
 const StyledLink = styled(Link)`
-    ${tw`flex mt-4 md:mt-12 mr-4 text-xl md:text-2xl leading-10 items-center justify-center lg:mx-auto`}
+    ${tw`flex mt-4 md:mt-12 mr-auto text-xl md:text-2xl leading-10 items-center justify-center`}
 `
 
-const StyledQuote = styled.p`
+const Quote = styled.p`
     ${tw`text-3xl md:text-5xl mt-5 font-bold`}
 `
 
@@ -42,14 +46,16 @@ export default function IndexFeatureSection ({ images }) {
             </MarkWrapper>
             <ImageOne>
                 <Img fluid={featureImage.childImageSharp.fluid} style={{maxWidth: `100%`}} />
-                <StyledQuote>
+            </ImageOne>
+            <QuoteWrapper>
+                <Quote>
                     “An agency with a steadfast commitment to protecting its people and its culture.”
-                </StyledQuote>
+                </Quote>
                 <StyledLink to='/'>
                     5 questions with Technical.ly
                     <Icon className={`ml-5`}/>
                 </StyledLink>
-            </ImageOne>
+            </QuoteWrapper>
             <ImageTwo>
                 <Img fluid={designTeam.childImageSharp.fluid} style={{maxWidth: `100%`}} />
             </ImageTwo>
