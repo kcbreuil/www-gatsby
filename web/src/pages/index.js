6ab16810af24c8ff3cb25e53f48288fbde8ab17f
asmith02 <asa.smith@verizonmedia.com>
2021-02-27 15:47:18 -0500

    Update index page

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,127 +1,83 @@
+
 import React from 'react';
 import { graphql } from 'gatsby';
-import styled from 'styled-components';
-
-// import Img from 'gatsby-image';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
-import HeroSlider from '../components/HeroSlider';
-import Panel from '../components/Panel';
-import GalleryPanel from '../components/GalleryPanel';
-import BlogTile from '../components/BlogTile';
+import Contact from '../components/Contact';
+import IndexCTA from '../components/IndexCTA';
+import IndexHero from '../components/IndexHero';
+import IndexFeatureSection from '../components/IndexFeatureSection';
+import IndexPressSection from '../components/IndexPressSection';
 
-const content = {
-  panelOne: {
-    header: `Backroom is a brand strategy and activation agency.`,
-    body: `Our clients stay ahead of consumer behaviors to create human-centered
-        experiences.`,
-  },
-  panelTwo: {
-    header: `We go beyond storytelling, helping brands listen first to create meaningful dialog with their customers.`,
-    link: `View our capabilities`,
-  },
-};
+export default function Index ({ data }) {
+    const {
+        barcoding,
+        designTeam,
+        featureImage,
+        image3,
+        tempRendia,
+    } = data
 
-const FlexColumn = styled.section`
-  display: flex;
-  flex-direction: column;
-
-  & header {
-    margin: auto;
-    max-width: 1280px;
-    width: 100%;
-
-    @media screen and (max-width: 450px) {
-      padding-left: 1rem;
-    }
-  }
-`;
-
-const FlexRow = styled.div`
-  display: flex;
-  margin: auto;
-  max-width: 1440px;
-
-  @media screen and (max-width: 450px) {
-    flex-direction: column;
-  }
-`;
-
-// const Test = styled.div`
-//   margin: auto;
-//   max-width: 90%;
-// `;
-
-export default function IndexPage(props) {
-  const {
-    data: {
-      datoCmsHomepage: { feature },
-      allSanityHpHero: { edges },
-    },
-  } = props;
-  // console.log(feature);
+    const images = [barcoding, tempRendia, image3]
 
   return (
-    <>
-      <Layout>
-        <SEO title="Home" />
-        <HeroSlider icons={edges} />
-        <Panel
-          isTitle="true"
-          header={content.panelOne.header}
-          body={content.panelOne.body}
-        />
-        <GalleryPanel />
-        <Panel header={content.panelTwo.header} link={content.panelTwo.link} />
-        <FlexColumn>
-          <header>
-            <h3 className="h3">Our opinions are free:</h3>
-          </header>
-          <FlexRow>
-            {feature.map(item => (
-              <BlogTile key={item.title} content={item} />
-            ))}
-          </FlexRow>
-        </FlexColumn>
-      </Layout>
-    </>
+    <Layout hasFooter hasBgColorTransition>
+      <SEO title="Page two" />
+      <IndexHero images={images} />
+      <IndexCTA />
+      <IndexFeatureSection images={[designTeam, featureImage]} />
+      <IndexPressSection />
+      <Contact />
+    </Layout>
   );
 }
 
 export const query = graphql`
-  query IndexQuery {
-    datoCmsHomepage {
-      feature {
-        title
-        body
-        linkHref
-        linkTitle
-      }
-    }
-    allSanityHpHero {
-      edges {
-        node {
-          _rawLogo
-          bgColor
-          excerpt
-          mobile: logo {
-            asset {
-              fluid(maxWidth: 400) {
-                ...GatsbySanityImageFluid_noBase64
-              }
+    query IndexQuery {
+        featureImage: file(relativePath: { regex: "/kara/" }) {
+            childImageSharp {
+                fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid
+                }
             }
-          }
-
-          desktop: logo {
-            asset {
-              fluid(maxWidth: 800) {
-                ...GatsbySanityImageFluid_noBase64
-              }
-            }
-          }
+            id
         }
-      }
+
+        barcoding: file(relativePath: {eq: "subway-takeover.jpg"}) {
+            childImageSharp {
+                fluid(maxWidth: 600) {
+                    ...GatsbyImageSharpFluid	
+                }
+            }
+            id
+        } 
+
+        tempRendia: file(relativePath: { regex: "/rendia/" }){
+            childImageSharp {	
+                fluid(maxWidth: 400) {	
+                    ...GatsbyImageSharpFluid	
+                }	
+            } 
+            id
+        }
+
+        image3: file(relativePath: { eq: "dcu-mobile-and-tiles.png" }){
+            childImageSharp {	
+                fluid(maxWidth: 600) {	
+                    ...GatsbyImageSharpFluid	
+                }	
+            } 
+            id
+        }
+
+        designTeam:   file(relativePath: {eq: "design-team.jpg"}) {
+            id
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid	
+                }
+            }
+        }
     }
-  }
 `;
