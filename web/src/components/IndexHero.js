4e22a0a9300131aa92b798ba34bce3e4fc10dc1f
asasmith <smith.asa.la@gmail.com>
2021-02-28 13:59:00 -0500

    Update copy

--- web/src/components/IndexHero.js
+++ web/src/components/IndexHero.js
@@ -43,21 +43,21 @@ export default function IndexHero ({ images }) {
                     <Img
                         fluid={images[0].childImageSharp.fluid}
                     />
-                    <StyledP>Statement about this project</StyledP>
+                    <StyledP>We collaborated with the Barcoding team to define a brand architecture and positioning that moved them beyond vertical lines and into the lynchpin of supply chain in an ever-changing technological world. An evolution of their existing logo showed a progression away from the barcode, while giving a nod to their original namesake with an elevated wordmark.</StyledP>
                 </ImageOne>
 
                 <ImageTwo to="/volo">
                     <Img
                         fluid={images[1].childImageSharp.fluid}
                     />
-                    <StyledP>Statement about this project</StyledP>
+                    <StyledP>We partnered with Volo City, a national brand that strengthens communities and impacts economic health through sports leagues. We unified each of their cities and the youth non-profit under a name that was inspired by the style of bocce that was played in their very first league.</StyledP>
                 </ImageTwo>
 
                 <ImageThree to="/dcunited">
                     <Img
                         fluid={images[2].childImageSharp.fluid}
                     />
-                    <StyledP>Statement about this project</StyledP>
+                    <StyledP>With a month before go-time, our team designed and launched a co-branded website to support the launch of D.C. United’s new stadium, Audi Field. The site was designed to capture the group energy anticipation of a game day in a digital space, with fly-through videos and a countdown timer to keep fans charged up.</StyledP>
                 </ImageThree>
             </StyledGrid>
         </StyledSection>
